ded15a37e957b469c8c33b824d1b97568f4a5eb1
pongruethaiv <viriyothai.p@husky.neu.edu>
2016-02-13 12:28:54 -0500

    Committing Assignment2

--- PrintingPatterns/TrianglePatterns/Program.cs
+++ PrintingPatterns/TrianglePatterns/Program.cs
@@ -16,22 +16,62 @@ namespace TrianglePatterns
 
 		static void DisplayPatternA()
 		{
-			// your implementation here
-		}
+            Console.WriteLine("Pattern A");
+            for (int i=1; i<=10; i++) //line number
+            {
+                for (int j=1; j<=i; j++) //number of stars
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("");
+            }
+        }
 
 		static void DisplayPatternB()
 		{
-			// your implementation here
-		}
+            Console.WriteLine("Pattern B");
+            for (int i=1; i<=10; i++)
+            {
+                for (int j=10; j>=i; j--)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("");
+            }
+        }
 
 		static void DisplayPatternC()
 		{
-			// your implementation here
-		}
+            Console.WriteLine("Pattern C");
+            for (int i=1; i<=10; i++)
+            {
+                for (int s=1; s<i; s++) //number of spaces
+                {
+                    Console.Write(" ");
+                }
+                for (int j=10; j>=i; j--)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("");
+            }
+        }
 
 		static void DisplayPatternD()
 		{
-			// your implementation here
-		}
+            Console.WriteLine("Pattern D");
+            for (int i=1; i<=10; i++)
+            {
+                for (int s=10; s>=i; s--)
+                {
+                    Console.Write(" ");
+                }
+                for (int j=1; j<=i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("");
+            }
+        }
 	}
 }
